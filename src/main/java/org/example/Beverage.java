6ef92bc0ca05fba2c3ca9b2b18cc59b26f79a55a
Farzaan Patwary <patwfk@farmingdale.edu>
2026-04-24 15:40:30 -0400

    added Beverage abstract class with shared fields and toString method for all beverage types

--- src/main/java/org/example/Beverage.java
+++ src/main/java/org/example/Beverage.java
@@ -1,4 +1,16 @@
 package org.example;
 
-public interface Beverage {
+public abstract class Beverage {
+
+    protected String size;
+    protected String milk;
+    protected String sweetener;
+    protected String temperature;
+
+    @Override
+    public String toString() {
+        return " | Size: " + size +
+                    (milk != null ? " | Milk: " + milk : "") +
+                    (sweetener != null ? " | Sweetener: " + sweetener : "");
+        }
 }
