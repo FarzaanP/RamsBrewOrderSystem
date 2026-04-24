866f12ccdae51416bf84db6eb0515dc20e743b54
Farzaan Patwary <patwfk@farmingdale.edu>
2026-04-24 19:39:56 -0400

    added Cappuccino class extending Beverage with size, milk, sweetener, shots fields and a toString method.

--- src/main/java/org/example/Cappuccino.java
+++ src/main/java/org/example/Cappuccino.java
@@ -1,4 +1,17 @@
 package org.example;
 
-public class Cappuccino {
+public class Cappuccino extends Beverage {
+    private int shots;
+
+    public Cappuccino(String size, String milk, String sweetener, int shots) {
+        this.size = size;
+        this.milk = milk;
+        this.sweetener = sweetener;
+        this.shots = shots;
+    }
+
+    @Override
+    public String toString() {
+        return "Cappuccino" + super.toString() + " | Shots: " + shots;
+    }
 }
