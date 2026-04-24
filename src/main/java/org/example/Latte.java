dcd3e368905dbda4e140ef0a2adc29efab5c6ba7
Farzaan Patwary <patwfk@farmingdale.edu>
2026-04-24 19:34:33 -0400

    added Latte class extending Beverage with shots field and all the other fields from beverage and a toString method.

--- src/main/java/org/example/Latte.java
+++ src/main/java/org/example/Latte.java
@@ -1,4 +1,18 @@
 package org.example;
 
-public class Latte {
+public class Latte extends Beverage {
+    private int shots;
+
+    public Latte(String temperature, String size, String milk, String sweetener, int shots) {
+        this.temperature = temperature;
+        this.size = size;
+        this.milk = milk;
+        this.sweetener = sweetener;
+        this.shots = shots;
+    }
+
+    @Override
+    public String toString() {
+        return temperature + " Latte" + super.toString() + " | Shots: " + shots;
+    }
 }
