b5cb87b412e6da405524c398955f6fedc82a7cde
Farzaan Patwary <patwfk@farmingdale.edu>
2026-04-24 19:47:31 -0400

    added Tea class extending Beverage with temp, size, milk, sweetener fields and a toString method for tea.

--- src/main/java/org/example/Tea.java
+++ src/main/java/org/example/Tea.java
@@ -1,4 +1,16 @@
 package org.example;
 
-public class Tea {
+public class Tea extends Beverage{
+
+    public Tea(String temperature, String size, String milk, String sweetener) {
+        this.temperature = temperature;
+        this.size = size;
+        this.milk = milk;
+        this.sweetener = sweetener;
+    }
+
+    @Override
+    public String toString() {
+        return temperature + " Tea" + super.toString();
+    }
 }
