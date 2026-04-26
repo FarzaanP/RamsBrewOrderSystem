3eb3668ab44f055a74db93f2e022f2d5cc100e32
Farzaan Patwary <patwfk@farmingdale.edu>
2026-04-25 22:45:56 -0400

    added BeverageBuilder interface with methods for setting size, milk, shots, sweetener, and temperature.

--- src/main/java/org/example/BeverageBuilder.java
+++ src/main/java/org/example/BeverageBuilder.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public interface BeverageBuilder {
+    BeverageBuilder setSize(String size);
+    BeverageBuilder setMilk(String milk);
+    BeverageBuilder setShots(int shots);
+    BeverageBuilder setSweetener(String sweetener);
+    BeverageBuilder setTemperature(String temperature);
+    Coffee getCoffee();
+    Latte getLatte();
+    Cappuccino getCappuccino();
+    Tea getTea();
 }
