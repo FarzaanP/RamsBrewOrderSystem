c1bfda0cba98ea79d100cefc8ea8eaf7aabf74f2
Farzaan Patwary <patwfk@farmingdale.edu>
2026-04-25 23:32:56 -0400

    added Order class that stores a beverage and an order type like dining in or takeout and assigns each order an ID, also added a toString method to print out the order details.

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public class Order {
+    private final Beverage beverage;
+    private final String orderType;
+    private static int counter = 1;
+    private final int orderId;
+
+    public Order(Beverage beverage, String orderType) {
+        this.beverage = beverage;
+        this.orderType = orderType;
+        this.orderId = counter++;
+    }
+
+    @Override
+    public String toString() {
+        return "Order #" + orderId + " [" + orderType + "]: " + beverage;
+    }
 }
